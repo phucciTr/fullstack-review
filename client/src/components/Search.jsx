ed0c1b4822f53d175e1435f21eea705e08762c56
phucciTr <miabam26@gmail.com>
2021-02-09 15:18:11 -0800

    Added diagrams and fixed bug in Search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       term: ''
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.search = this.search.bind(this);
   }
 
   onChange (e) {
@@ -21,9 +27,9 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>
       <button onClick={this.search}> Add Repos </button>
-    </div>) 
+    </div>)
   }
 }
 
