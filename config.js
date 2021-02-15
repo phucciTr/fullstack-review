f25dcbb9eec84f71549d9fd8aa4e1b459c038351
phucciTr <miabam26@gmail.com>
2021-02-14 21:36:33 -0800

    Updated gitignore

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  TOKEN: 'bb35fa08ee4cbe68b6e562c134ea619e432156ab',
-  MONGODB_URI: 'mongodb+srv://phucci:phucci@cluster0.qrckp.mongodb.net/fetcher?retryWrites=true&w=majority'
-};
