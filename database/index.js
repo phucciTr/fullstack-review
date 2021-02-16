4f6e9c4c2c01377b61e49368a1836ba75ddb5016
phucciTr <miabam26@gmail.com>
2021-02-15 17:56:20 -0800

    Test

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fetcher', {
+mongoose.connect('mongodb://localhost/fetcher', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
